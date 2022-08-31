16402d907fe1679b9b8b7fd0266b8d658a204e52
bsquintana55 <bsquintana55@misena.edu.co>
2022-08-31 17:52:27 -0500

    validadors

---
+++ app/Http/Controllers/BootcampController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Bootcamp;
+use App\Http\Requests\StoreBootcampRequest;
+use App\Http\Resources\BootcampResource;
+use App\Http\Resources\BootcampCollection;
+
+
+class BootcampController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+      //metodos json
+      //parametros: 1. data a enviar al client
+      //2. codigo de status
+        return response()->json(  
+        new BootcampCollection(Bootcamp::all()),
+        200);
+        
+    }
+   // []
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+
+
+
+
+    public function store(StoreBootcampRequest $request)
+    {
+        //1. Traer el payload 
+        //2. Crea el nuevo bootcamp
+        return response()->json(["success" => true, 
+        "data"=> new BootcampResource 
+        (Bootcamp::create($request->all())),
+
+        201]
+    );
+    }
+
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return response()->json( [ "success"=>true,
+                                    "data"=> new BootcampResource (Bootcamp::find($id))
+                                 ]  ,200);
+    }    
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+
+    public function update(Request $request, $id)
+    {
+       //1. localizar  el bootcamp con id
+       $b = Bootcamp::find($id);
+        // actualizar
+        $b->update($request->all());
+
+        return response()->json( [ "success"=>true,
+       "data"=> new BootcampResource ($b)
+                                ]  , 201);
+    }
+
+
+    
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+
+        $b=Bootcamp::find($id);
+        $b->delete();
+        return response()->json( [ "success"=>true,
+        "data"=> new BootcampResource ($b)
+     ]  , 201);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BootcampController;
+use App\Http\Controllers\CourseController;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+  //  return $request->user();
+//});
+
+Route::get('prueba', function(){
+    echo"hola";
+});
+
+//ruta para la gestion de estados
+//de los bootcmps
+
+Route::apiResource('bootcamps', BootcampController::class);
+Route::post('courses/{idbootcamp}/create', [CourseController::class, "store"]);
+
